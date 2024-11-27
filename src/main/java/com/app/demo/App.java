69549085dba6183c384de95462e930a9e8d05b2f
Dhanush52 <kummaridhanush9143@gmail.com>
2024-11-27 11:18:09 +0530

    Update App.java

--- src/main/java/com/app/demo/App.java
+++ src/main/java/com/app/demo/App.java
@@ -6,5 +6,6 @@ package com.app.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Dhanush 22bd1a0552");
+         System.out.println("Hello World!");
     }
 }
